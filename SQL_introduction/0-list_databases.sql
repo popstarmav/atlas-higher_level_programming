4ee059a73ddfe7371a0c0b0cc6353e2b64273a92
popstarmav <cj.johnson2017@gmail.com>
2024-06-16 19:22:00 +0000

    list all databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,2 +1,5 @@
--- List databases of my MySQL server in order 
-SHOW DATABASES
+-- 0-list_databases.sql
+-- This script lists all databases on the MySQL server
+
+-- List all databases and sort them alphabetically
+SHOW DATABASES ORDER BY `Database`;
