eb49e6e8da91e28f20fadc7c925bfa25bf85b278
popstarmav <cj.johnsno2017@gmail.com>
2024-06-17 19:25:52 +0000

    creates the database hbtn_0d_2 and the user user_0d_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,3 +3,14 @@
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
+
+-- Check if user_0d_2 exists
+SELECT
+    CASE WHEN EXISTS (
+        SELECT 1
+        FROM mysql.user
+        WHERE user = 'user_0d_2' AND host = 'localhost'
+    ) THEN 'Correct output: user_0d_2 exists'
+    ELSE 'Correct output: user_0d_2 doesn’t exist'
+    END AS result
