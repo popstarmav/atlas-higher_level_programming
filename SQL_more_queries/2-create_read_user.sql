f2c695154d557c8de753d552f66ba0a3d55afda3
popstarmav <cj.johnson2017@gmail.com>
2024-06-18 14:38:01 +0000

    create user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,5 @@
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+SELECT CASE WHEN EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_2') THEN 'user_0d_2 exists' ELSE 'user_0d_2 doesn’t exist' END;
 FLUSH PRIVILEGES;
