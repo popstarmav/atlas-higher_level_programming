2711310fd52915d22aaeace079fae67848018bfd
popstarmav <cj.johnson2017@gmail.com>
2024-06-18 16:45:43 +0000

    list numbers of shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Listed all genres and display numbers of shows
+
+SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
