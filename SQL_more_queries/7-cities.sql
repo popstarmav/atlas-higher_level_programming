853f9c0b1b80621f6ba8876c0199052383553c96
popstarmav <cj.johnson2017@gmail.com>
2024-06-18 15:18:48 +0000

    creates a database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Create the cities table
+CREATE TABLE IF NOT EXISTS cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states (id)
+);
