ecbcfd380d39c929987d78bcf2cf183fb636e9a1
popstarmav <cj.johnson2017@gmail.com>
2024-06-18 03:10:30 +0000

    select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-import sys 
+import sys
 import MySQLdb
 
 def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
     mysql_username = sys.argv[1]
-    mysql_username = sys.argv[2]
+    mysql_password = sys.argv[2]
     db_name = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost", 
-        port=3306, 
-        user=mysql_username, 
-        passwd=mysql_password, 
-        db=db_name)
-    
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+        cursor.close()
+        db.close()
 
-    states = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
-    for state in states:
-        print(state)
-    
-    cursor.close()
-    db.close()
+if __name__ == "__main__":
+    main()
