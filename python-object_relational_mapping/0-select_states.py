c838c615dc78780d0baf31515e606e0028f6e60c
popstarmav <cj.johnson2017@gmail.com>
2024-06-19 03:07:43 +0000

    lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,13 @@
 import sys
 import MySQLdb
 
+
 def main():
     if len(sys.argv) != 4:
         print("Usage: ./0-select_states.py <mysql_username> <mysql_password> "
               "<database_name>")
         sys.exit(1)
-        
+
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
@@ -27,13 +28,14 @@ def main():
 
         for state in states:
             print(state)
-        
+
         cursor.close()
         db.close()
 
     except MySQLdb.Error as e:
         print(f"Error: {e}")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
