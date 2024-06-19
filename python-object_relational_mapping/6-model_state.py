094d0d1d8cf31bf9d70f4eaac9ef8710477010cc
popstarmav <cj.johnson2017@gmail.com>
2024-06-19 04:45:32 +0000

    python file that contains the class definition of a State and an instance

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
+"""Start link class to table in database"""
 import sys
-from model_state import Base, State
+from model_state import Base  # Removed unused import
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Create the engine with MySQL connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create all tables in the database
     Base.metadata.create_all(engine)
