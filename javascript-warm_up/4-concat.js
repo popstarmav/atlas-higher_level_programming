0f409d06f9b929c23fda64044510bb7a1bab240f
popstarmav <cj.johnson2017@gmail.com>
2024-07-22 10:09:45 -0500

    prints two arguments passed to it, in the following format: “ is ”

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 
 // Get the command-line arguments excluding the first two (node and script path)
-const args = process.argv.slice(2);
+const [, , arg1, arg2] = process.argv;
 
-// Print the arguments in the format " is " if there are at least two arguments
-if (args.length >= 2) {
-  const [arg1, arg2] = args;
-  console.log(`${arg1} is ${arg2}`);
-} else {
-  console.log('Not enough arguments');
-}
+// Print the arguments in the format " is "
+console.log(`${arg1} is ${arg2}`);
