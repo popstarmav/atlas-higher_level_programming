55217340b24cd428c0728d69c794f57028313d2a
popstarmav <cj.johnson2017@gmail.com>
2024-07-22 09:58:14 -0500

    prints two arguments passed to it, in the following format: “ is ”

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Using const for constant values
+const arg1 = process.argv[2];
+
+// Checking if any argument is passed and printing the result
+if (args.length >= 2) {
+    console.log(~`${arg1} is ${arg2}`);
+} else {
+    console.log('No argument');
+}
