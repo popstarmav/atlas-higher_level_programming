5a83e283a62dfc1429b2ca5aa90f7c9fb9ebd61b
popstarmav <cj.johnson2017@gmail.com>
2024-07-22 01:09:38 -0500

    myVar holds the value and console.log 1,2,3 print the outcome

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// Define a variable named myVar with
+// Print the value of myVar
+const myVar1 = 'C is fun';
+const myVar2 = "Python is cool";
+const myVar3 = "JavaScript is amazing";
+
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
