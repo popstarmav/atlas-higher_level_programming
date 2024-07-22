a67cf1eb60321af86164dbfb0034e28ff01404dc
popstarmav <cj.johnson2017@gmail.com>
2024-07-22 15:25:29 -0500

    returns the addition of 2 integers

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Define the add function
 const add = require('./13-add').add;
-console.log(add(3, 5))
-console.log(add(4, 85))
-console.log(add(93, -4))
-console.log(add(0, 89))
+console.log(add(3, 5));
+console.log(add(4, 85));
+console.log(add(93, -4));
+console.log(add(0, 89));
