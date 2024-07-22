781f712b774369e0d94d8e9f4b97af3917b9b90e
popstarmav <cj.johnson2017@gmail.com>
2024-07-22 14:10:03 -0500

    computes and prints a factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 function factorial (n) {
-    if (isNaN(n) || n <= 1) {
-      return 1;
-    }
-    return n * factorial(n - 1);
+  if (isNaN(n) || n <= 1) {
+    return 1;
   }
-  
-  const num = parseInt(process.argv[2]);
-  console.log(factorial(num));
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
