666a23a1f050099a7c2d5b91ac9e6040491e4009
popstarmav <cj.johnson2017@gmail.com>
2024-07-22 18:31:56 -0500

    prints the rectangle using the character X

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (width, height) {
-      if (width > 0 && height > 0) {
-        this.width = width;
-        this.height = height;
-      } else {
-        this.width = null;
-        this.height = null;
-      }
-    }
-  
-    print () {
-      if (this.width === null || this.height === null) return;
-  
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    } else {
+      this.width = null;
+      this.height = null;
     }
   }
-  
-  module.exports = Rectangle;
-  
+
+  print () {
+    if (this.width === null || this.height === null) return;
+
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
