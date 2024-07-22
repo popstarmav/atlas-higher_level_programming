d7c81a0dd7100ee198a45095e65fcfc41ec20eb3
popstarmav <cj.johnson2017@gmail.com>
2024-07-22 10:06:14 -0500

    prints two arguments passed to it, in the following format: “ is ”

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,11 +1,12 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
-// Using const for constant values
-const arg1 = process.argv[2];
+// Get the command-line arguments excluding the first two (node and script path)
+const args = process.argv.slice(2);
 
-// Checking if any argument is passed and printing the result
+// Print the arguments in the format " is " if there are at least two arguments
 if (args.length >= 2) {
-    console.log(~`${arg1} is ${arg2}`);
+  const [arg1, arg2] = args;
+  console.log(`${arg1} is ${arg2}`);
 } else {
-    console.log('No argument');
+  console.log('Not enough arguments');
 }
