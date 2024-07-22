edd1c19df8a4feaf79650f86e46c7d7be02bdd18
popstarmav <cj.johnson2017@gmail.com>
2024-07-22 14:30:47 -0500

    searches the second biggest integer in the list of arguments

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 
 function findSecondBiggest (args) {
-    if (args.length <= 1) {
-      return 0;
-    }
-  
-    const numbers = args.map(Number);
-    const uniqueNumbers = [...new Set(numbers)];
-  
-    if (uniqueNumbers.length === 1) {
-      return 0;
-    }
-  
-    uniqueNumbers.sort((a, b) => b - a);
-    return uniqueNumbers[1];
+  if (args.length <= 1) {
+    return 0;
   }
-  
-  const args = process.argv.slice(2);
-  console.log(findSecondBiggest(args));
+
+  const numbers = args.map(Number);
+  const uniqueNumbers = [...new Set(numbers)];
+
+  if (uniqueNumbers.length === 1) {
+    return 0;
+  }
+
+  uniqueNumbers.sort((a, b) => b - a);
+  return uniqueNumbers[1];
+}
+
+const args = process.argv.slice(2);
+console.log(findSecondBiggest(args));
