2b5b34c5640b3384720116df17f31f6dde3d4e5a
popstarmav <cj.johnson2017@gmail.com>
2024-07-23 03:24:22 -0500

    defines a rectangle

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,39 +1,38 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (width, height) {
-      if (width <= 0 || height <= 0) {
-        this.width = null;
-        this.height = null;
-      } else {
-        this.width = width;
-        this.height = height;
-      }
-    }
-  
-    print () {
-      if (this.width === null && this.height === null) return;
-  
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  
-    rotate () {
-      if (this.width !== null && this.height !== null) {
-        const aux = this.width;
-        this.width = this.height;
-        this.height = aux;
-      }
-    }
-  
-    double () {
-      if (this.width !== null && this.height !== null) {
-        this.width *= 2;
-        this.height *= 2;
-      }
+  constructor (width, height) {
+    if (width <= 0 || height <= 0) {
+      this.width = null;
+      this.height = null;
+    } else {
+      this.width = width;
+      this.height = height;
     }
   }
-  
-  module.exports = Rectangle;
-  
+
+  print () {
+    if (this.width === null && this.height === null) return;
+
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    if (this.width !== null && this.height !== null) {
+      const aux = this.width;
+      this.width = this.height;
+      this.height = aux;
+    }
+  }
+
+  double () {
+    if (this.width !== null && this.height !== null) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+module.exports = Rectangle;
