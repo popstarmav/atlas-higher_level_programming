c8fae07d579eb9807e9160c9191340725d10328a
popstarmav <cj.johnson2017@gmail.com>
2024-07-25 23:10:06 -0500

    script that reads and prints the content of a file.

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,13 +5,12 @@ const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-    if (err) {
-        console.error(`{ 
+  if (err) {
+    console.error(`{ 
             Error: ${err.code}: ${err.message}\n  at Error (native)\n  
             errno: ${err.errno},\n  code: '${err.code}',\n  
             syscall: '${err.syscall}',\n  
             path: '${err.path}' 
 }`);
-        return;
-    }
+  }
 });
