9c33894ab9f4a99f246ecb42518b2c0b3ad0f497
popstarmav <cj.johnson2017@gmail.com>
2024-07-25 22:36:14 -0500

    script that reads and prints the content of a file.

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,15 +4,15 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(`{ 
+  if (err) {
+    console.error(`{ 
             Error: ${err.code}: ${err.message}\n    
             at Error (native)\n  
             errno: ${err.errno},\n  
             code: '${err.code}',\n  
             syscall: '${err.syscall}',\n  
             path: '${err.path}' }`);
-        return;
-    }
-    console.log(data);
+    return;
+  }
+  console.log(data);
 });
