5297d67c7cceb01a7a871b8b78d1ba9b73cede70
popstarmav <cj.johnson2017@gmail.com>
2024-07-25 22:52:36 -0500

    write a string to file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+    if (err) {
+        console.error(`{ 
+            Error: ${err.code}: ${err.message}\n    at Error (native)\n  
+            errno: ${err.errno},\n  
+            code: '${err.code}',\n  
+            syscall: '${err.syscall}',\n  
+            path: '${err.path}' 
+}`);
+        return;
+    }
+    console.log('File written successfully');
+});
