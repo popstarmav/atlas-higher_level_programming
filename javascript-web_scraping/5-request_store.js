874d8edb85b802ef5a9f837255bef1711fb4420f
popstarmav <cj.johnson2017@gmail.com>
2024-07-26 11:08:41 -0500

    script that gets the contents of a webpage and stores it in a file.

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(`Error: ${err.message}`);
+      }
+    });
+  }
+});
