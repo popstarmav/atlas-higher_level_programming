c9293bc6d21faeea4852b3a8517f6706b40e023b
popstarmav <cj.johnson2017@gmail.com>
2024-07-25 23:01:48 -0500

    file with text file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const fs = require('fs');
 const filePath = process.argv[2];
@@ -7,13 +7,11 @@ const stringToWrite = process.argv[3];
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
     if (err) {
         console.error(`{ 
-            Error: ${err.code}: ${err.message}\n    at Error (native)\n  
-            errno: ${err.errno},\n  
-            code: '${err.code}',\n  
+            Error: ${err.code}: ${err.message}\n  at Error (native)\n  
+            errno: ${err.errno},\n  code: '${err.code}',\n  
             syscall: '${err.syscall}',\n  
             path: '${err.path}' 
 }`);
         return;
     }
-    console.log('File written successfully');
 });
