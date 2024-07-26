a602e92290eec8180074cf67c5e0e483a0b6bd9f
popstarmav <cj.johnson2017@gmail.com>
2024-07-25 23:49:55 -0500

    scprit that print SStarWars movies by number matches given by int

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
